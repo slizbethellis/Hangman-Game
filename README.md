894d3a5e12472ce225b32eb081c8a0cdac1cd768
slizbethellis <slizbethellis@gmail.com>
2018-02-06 18:57:03 -0600

    added features to logic, but style of original code maintained

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Hangman-Game
-A game of Hangman
+A game of Hangman built using vanilla JavaScript (no jQuery), and Bootstrap 3 with minimal CSS (only used to control size of one image field). The words are names of sheep breeds, and the game shows you a photo of that sheep breed when the round is finished.
+
+Play the game here: https://slizbethellis.github.io/Hangman-Game/
+
+An example of a finished round:
+
+![Sheep Hangman](/sheep-hangman.png?raw=true)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// game variables;
 var wins = 0;
 var losses = 0;
 var guesses = 0;
@@ -6,6 +7,7 @@ var word = null;
 var hiddenWord = null;
 var finished = null;
 var newRound = true;
+var guessedLetters = [];
 
 var words =[
 "columbia",
@@ -19,57 +21,123 @@ var words =[
 "targhee"];
 
 document.onkeyup = function(event) {
-  keyPress = event.key;
+	keyPress = event.key;
+	// starts game
   if ((event.keyCode >= 65 && event.keyCode <= 90) || (event.keyCode >= 97 && event.keyCode <= 122)) {
     determineRound();
   }
 
-
+	// updates win count
   function updateWins() {
   	var winsElement = document.getElementById("wins");
   	winsElement.innerHTML = wins;
-  	updateGuesses();
+		updateGuesses();
+		correctWord();
   }
 
+	// updates loss count
   function updateLosses() {
   	var lossesElement = document.getElementById("losses");
   	lossesElement.innerHTML = losses;
-  	updateGuesses();
+		updateGuesses();
+		wrongWord();
   }
 
+	// updates number of guesses
   function updateGuesses() {
   	var guessesElement = document.getElementById("guesses");
-  	guessesElement.innerHTML = guesses;
-  }
-
-  function clearBox() {
-		  document.getElementById("word").innerHTML = "";
-		  document.getElementById("letters").innerHTML = "";
+		guessesElement.innerHTML = guesses;
+		updateHeader();
 	}
-  
+	
+	// update panel heading with appropriate directions
+	function updateHeader() {
+		var headerElement = document.getElementById("header");
+		if (newRound === false) {
+			headerElement.innerHTML = "Game in Progress";
+		}
+		else {
+			headerElement.innerHTML = "Press Any Letter Key to Start New Round";
+		}
+	}
+
+	// clears current word area and guessed letters, starts new round
+  function clearBox() {
+		document.getElementById("word").innerHTML = "";
+		document.getElementById("letters").innerHTML = "";
+		guessedLetters = [];
+		newRound = false;
+		createNewWord();
+	}
+	
+	// generates a random word 
   function createNewWord() {
   	word = words[Math.floor(Math.random() * words.length)];
-  	guesses = (word.length * 2) - 1;
-  }
+		guesses = (word.length * 2) - 1;
+		createBlankWord();
+	}
+	
+	// congratulates user
+	function correctWord() {
+		var wordElement = document.getElementById("word");
+		var newDiv = document.createElement("p");
+		var wordDiv = document.createElement("p");
+		wordDiv.innerHTML = "<strong>YOU WIN!</strong>";
+		wordElement.appendChild(newDiv);
+		wordElement.appendChild(wordDiv);
+		showSheep();
+	}
 
-  function changeArray(letter, i) {
-  	console.log(letter);
-  	console.log(hiddenWord[i]);
-  	hiddenWord[i] = letter;
-  }
+	// shows correct word and picture of sheep breed
+  function wrongWord() {
+		var wordElement = document.getElementById("word");
+		var newDiv = document.createElement("p");
+		var wordDiv = document.createElement("text");
+		var loseDiv = document.createElement("p");
+		loseDiv.innerHTML = "<strong>YOU LOSE!</strong>";
+		wordDiv.innerHTML = "The correct word is: <i>" + word + "</i>";
+		wordElement.appendChild(newDiv);
+		wordElement.appendChild(loseDiv);
+		wordElement.appendChild(wordDiv);
+		showSheep();
+	}
 
+	// displays sheep breed at end of round
+	function showSheep() {
+		var lettersElement = document.getElementById("letters");
+		var newDiv = document.createElement("p");
+		var sheepDiv = document.createElement("img");
+		sheepDiv.id = "word-pic";
+		sheepDiv.src = "assets/images/" + word + ".jpg";
+		lettersElement.appendChild(newDiv);
+  	lettersElement.appendChild(sheepDiv);
+	}
+
+	// creates blank spaces in current word area
   function createBlankWord() {
   	hiddenWord = [];
 	  for (var i = 0; i < word.length; i++){
-	    	hiddenWord[i] = "_";
-	    	var newDiv = document.createElement("text");
-	    	newDiv.id = "letter" + i;
-	    	newDiv.innerHTML = hiddenWord[i] + " ";
-	    	var wordElement = document.getElementById("word");
-	    	wordElement.appendChild(newDiv);
-	  }
+			hiddenWord[i] = "_";
+			var newDiv = document.createElement("text");
+			newDiv.id = "letter" + i;
+			newDiv.innerHTML = hiddenWord[i] + " ";
+			var wordElement = document.getElementById("word");
+			wordElement.appendChild(newDiv);
+		}
+		validateLetter();
   }
-
+	
+	// validates keypress; rejects previously typed letters
+	function validateLetter() {
+		if (!guessedLetters.includes(keyPress.toLowerCase())) {
+			guessedLetters.push(keyPress.toLowerCase());
+			determineLetter();
+		}
+		else {
+			alert("You have already guessed that letter!");
+		}
+	}
+	// determines if guessed letter matches or not
   function determineLetter() {
   	for (var i = 0; i < word.length; i++) {
   		if (word[i] === keyPress) {
@@ -80,14 +148,15 @@ document.onkeyup = function(event) {
   	}
   	console.log("Hello!");
   	guesses--;
-  	updateGuesses();
+		updateGuesses();
   	var letterDiv = document.createElement("text");
   	letterDiv.innerHTML = keyPress + " ";
   	var lettersElement = document.getElementById("letters");
   	lettersElement.appendChild(letterDiv);
-  	newRound = false;
+  	determineIfFinished();
   }
 
+	// determines if round is finished
   function determineIfFinished() {
   	var spaces = 0;
   	for (var i = 0; i < word.length; i++) {
@@ -96,31 +165,27 @@ document.onkeyup = function(event) {
   		}
   	}
   	if (guesses === 0) {
-  		losses++;
+			losses++;
+			newRound = true;
   		updateLosses();
-  		newRound = true;
   	}
   	else if (spaces === 0) {
-  		wins++;
+			wins++;
+			newRound = true;
   		updateWins();
-  		newRound = true;
-  		clearBox();
   	}
   	else {
-  		newRound = false;
+			newRound = false;
   	}
   }
 
+	// control function that calls other functions based on whether or not it is a new round
   function determineRound() {
   	if (newRound === true) {
-  		createNewWord();
-  		createBlankWord();
-  		determineLetter();
-  		guesses = (word.length * 2) - 1;
+  		clearBox();
   	}
   	else {
-  		determineLetter();
-  		determineIfFinished();
+  		validateLetter();
   	}
   }
 };
